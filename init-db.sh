e4f236f5e4030e7fa709bbf8bdc8924bfe7b0a65
Your Name <you@example.com>
2026-03-27 12:07:34 +0530

    Fixed Django Allowed Hosts and uWSGI port

--- init-db.sh
+++ init-db.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -e
 
+# Check if DATABASE exists and create if NOT
+create_db_if_not_exists() {
+  local db_name=$1
+  if psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "$db_name"; then
+    echo "Database '$db_name' already exists."
+  else
+    echo "Creating database '$db_name'..."
+    createdb -U "$POSTGRES_USER" "$db_name"
+  fi
+}
+
+create_db_if_not_exists "sonarqube"
+create_db_if_not_exists "defectdojo"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE sonarqube;
-    CREATE DATABASE defectdojo;
     GRANT ALL PRIVILEGES ON DATABASE sonarqube TO postgres;
     GRANT ALL PRIVILEGES ON DATABASE defectdojo TO postgres;
 EOSQL
